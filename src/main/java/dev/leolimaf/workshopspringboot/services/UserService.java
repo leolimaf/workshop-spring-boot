3efb76c51f2aa37c7c64066cdd842f7ba7b035a2
Leonardo Lima <leolimaf@outlook.com>
2021-01-29 11:42:34 -0300

    Exception handling - delete

--- src/main/java/dev/leolimaf/workshopspringboot/services/UserService.java
+++ src/main/java/dev/leolimaf/workshopspringboot/services/UserService.java
@@ -2,8 +2,11 @@ package dev.leolimaf.workshopspringboot.services;
 
 import dev.leolimaf.workshopspringboot.entities.User;
 import dev.leolimaf.workshopspringboot.repositories.UserRepository;
+import dev.leolimaf.workshopspringboot.services.exceptions.DatabaseException;
 import dev.leolimaf.workshopspringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
