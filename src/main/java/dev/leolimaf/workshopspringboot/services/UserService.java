3478851bd1c4df9c5fddaaa642387bfced3cef4f
Leonardo Lima <leolimaf@outlook.com>
2021-01-29 11:18:57 -0300

    Exception handling - findById

--- src/main/java/dev/leolimaf/workshopspringboot/services/UserService.java
+++ src/main/java/dev/leolimaf/workshopspringboot/services/UserService.java
@@ -2,6 +2,7 @@ package dev.leolimaf.workshopspringboot.services;
 
 import dev.leolimaf.workshopspringboot.entities.User;
 import dev.leolimaf.workshopspringboot.repositories.UserRepository;
+import dev.leolimaf.workshopspringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
